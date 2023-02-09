e195bf6e92308d532b9c763319e91f6c844300d4
meganlee088 <megan.rose.lee@intel.com>
2023-02-09 14:26:33 -0800

    change

--- examples/fullekscluster/main.tf
+++ examples/fullekscluster/main.tf
@@ -1,23 +1,28 @@
 provider "aws" {
   region = "us-west-2"
+  access_key = var.my_access_key
+  secret_key = var.my_secret_key
 }
 
-module "eks_example_complete" {
-  source = "terraform-aws-modules/eks/examples/complete"
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.7.0"
 
   cluster_name = "my-eks-cluster"
 
   tags = {
-    Terraform = "True"
-    Environment = "dev"
+    Terraform   = "true"
+    Environment = "test"
   }
 
+  # EC2 instance type
   worker_groups_launch_template = [
     {
-      instance_type = "m6i.large"
-      asg_desired_capacity = 2
+      instance_type = var.instance_type
     }
   ]
 
-  vpc_enabled = true
+  vpc_create     = true
+  vpc_cidr       = "10.0.0.0/16"
+  public_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
--- examples/fullekscluster/variables.tf
+++ examples/fullekscluster/variables.tf
@@ -2,17 +2,24 @@
 ####     Intel      ####
 ########################
 
+variable "instance_type"{}
+
 ########################
 ####    Required    ####
 ########################
-variable "instance_type"{}
+
 variable "subnets"{
     default = ["subnet-049df61146f12", "subnet-049df61146f13", "subnet-049df61146f14"]
 }
+
+variable "my_access_key"{
+    default = "AKIAXI2GL3V4CVR6U2HJ"
+}
+
+variable "my_secret_key"{
+    default = "7uW9fOiTe1jfLUFOmWs/m956sYb4c2+qzG4lg7Hi"
+}
+
 ########################
 ####     Other      ####
 ########################
-variable "version"{
-    type = number
-    default= "19.7.0"
-}
