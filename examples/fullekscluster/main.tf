130c8799ca4dfbf2bc9e95231495ef94558ee596
meganlee088 <megan.rose.lee@intel.com>
2023-02-14 08:59:29 -0800

    change to work

--- examples/fullekscluster/main.tf
+++ examples/fullekscluster/main.tf
@@ -1,31 +1,8 @@
 provider "aws" {
   region = "us-west-2"
-  access_key = var.my_access_key
-  secret_key = var.my_secret_key
 }
 
-resource "aws_launch_template" "worker_group_template" {
-  name_prefix = "worker-group-template"
-
-  launch_template_data = {
-    instance_type = var.instance_type
-    # Other launch template configuration such as block device mapping, security groups, etc.
-  }
-}
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
+module "eks_example_complete" {
+  source  = "terraform-aws-modules/eks/aws//examples/complete"
   version = "19.7.0"
-
-  cluster_name = "my-eks-cluster"
-
-  tags = {
-    Terraform   = "true"
-    Environment = "test"
-  }
-
-  worker_groups_launch_template = [
-    {
-      launch_template_id = aws_launch_template.worker_group_template.id
-    }
-  ]
 }
