3d29c4b26f80ecf8fe0534657b9bacdd970a5301
Petar Torre <petar.torre@intel.com>
2023-12-21 23:27:20 +0100

    compute-intensive

--- Examples/compute-intensive/vpc.tf
+++ Examples/compute-intensive/vpc.tf
@@ -6,7 +6,6 @@
 module "vpc" {
   source     = "terraform-aws-modules/vpc/aws"
   create_vpc = !local.use_existing_vpc
-  #count           = local.use_existing_vpc ? 0 : 1
   name            = "${local.new_vpc_name_prefix}-${random_string.random_suffix.result}"
   cidr            = local.new_vpc_cidr
   azs             = ["${data.aws_region.current.name}a", "${data.aws_region.current.name}b", "${data.aws_region.current.name}c", "${data.aws_region.current.name}d"]
